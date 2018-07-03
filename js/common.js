c237025ff3f763b7043a886bc28179fa8c9f05b4
mysticheskiy <zelatron@mail.ru>
2018-07-03 16:18:55 +0500

    js

---
+++ js/common.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	var clock;
+	clock = $(".clock").FlipClock({
+		clockFace : "DailyCounter",
+		autoStart : false,
+		callbacks : {
+			stop : function() {
+				$("message").html("Time out");
+			}
+		}
+	});
+});
+
+var dt = "10 23 2018 15:00:00";
+var first = new Date(dt);
+var lasr = Date.now();
+var remaining = first - last;
+remaining /=1000
+
+clock.setTime(remaining);
+clock.setCountdown(true);
+clock.start();
